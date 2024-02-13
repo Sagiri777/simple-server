3c5ccf5cc59d8212fe947dffd6629109b9ded7d3
Sagiri777 <57796392+Sagiri777@users.noreply.github.com>
2024-02-13 23:06:00 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,14 +1,27 @@
 from twitterscraper import query_tweets#name-twitterscraper
+from openpyxl import Workbook#name-openpyxl
 
-if __name__ == '__main__':
-    #list_of_tweets = query_tweets("里神咕", 10)
+# 搜索用户的推文，不使用代理
+tweets = query_tweets("from:gugugulee", limit=15, proxy=None)
 
-    #print the retrieved tweets to the screen:
-    for tweet in query_tweets("里神咕", 15):
-        print(tweet)
+# 创建工作簿和工作表
+wb = Workbook()
+ws = wb.active
 
-    #Or save the retrieved tweets to file:
-    file = open("output.txt", "w")
-    for tweet in query_tweets("里神咕", 10):
-        file.write(str(tweet.text.encode('utf-8')))
-    file.close()
+# 添加表头
+ws.append(['Timestamp', 'Username', 'Text', 'Media'])
+
+# 遍历推文并写入工作表和输出至终端
+for tweet in tweets:
+    media_urls = ', '.join([media['expanded_url'] for media in tweet.media]) if tweet.media else ''
+    row_data = [tweet.timestamp, tweet.username, tweet.text, media_urls]
+    ws.append(row_data)
+    print(f'Timestamp: {row_data[0]}, Username: {row_data[1]}, Text: {row_data[2]} 媒体链接: {row_data[3]} ٩(◕‿◕｡)۶')
+
+# 指定保存的Excel文件名
+excel_file = "gugugulee_tweets.xlsx"
+
+# 保存工作簿至Excel文件
+wb.save(excel_file)
+
+print(f'推文和媒体链接已保存至 {excel_file} 文件中！')
